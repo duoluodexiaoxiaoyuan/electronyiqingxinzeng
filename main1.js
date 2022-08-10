fcd08a4d0ef9f4304b6f138148f38bdbe4542c1b
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-08-10 21:34:24 +0800

    electron 透明窗口可移动功能的实现

--- main1.js
+++ main1.js
@@ -1,21 +1,17 @@
 const { app, BrowserWindow } = require("electron");
 function createWindow() {
   let win = new BrowserWindow({
-    width: 500,
-    height: 500,
-    x: 1300,    // 相对于左侧的偏移量
+    width: 300,
+    height: 220,
+    x: 100,
     y: 50,
     // frame: false,
-    // focusable: false,  // 一直置顶
     // titleBarStyle: "hidden",
-    // transparent: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  // win.setIgnoreMouseEvents(true)  点击穿透，可以点到下面你开启的应用的功能
-  // win.setIgnoreMouseEvents(true)
-  win.loadFile("./bibiliFanTableBar.html");
+  win.loadFile("./index.html");
   // win.loadURL(
   //   "https://c.m.163.com/ug/api/wuhan/app/data/list-total?t=329668874965"
   // );
